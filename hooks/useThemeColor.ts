79126c6b294ae3fced0d75ee8feece209ea7306d
TamirisLinard <102989908+TamirisLinard@users.noreply.github.com>
2024-11-21 09:22:21 -0300

    Update useThemeColor.ts

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -1,17 +1,23 @@
 import { useColorScheme } from 'react-native';
-
 import { Colors } from '@/constants/Colors';
+
+type Theme = 'light' | 'dark';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
 ) {
-  const theme = useColorScheme() ?? 'light';
+  // Obter o tema atual, com fallback para 'light' caso não seja detectado
+  const theme: Theme = useColorScheme() ?? 'light';
+
+  // Tentar pegar a cor diretamente das props
   const colorFromProps = props[theme];
 
+  // Se a cor foi definida nas props, retornar essa cor
   if (colorFromProps) {
     return colorFromProps;
-  } else {
-    return Colors[theme][colorName];
   }
+
+  // Caso contrário, retornar a cor padrão do tema (light ou dark)
+  return Colors[theme][colorName];
 }
