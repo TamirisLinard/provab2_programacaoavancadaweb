7826ebb5fb7b290c03df896923060e6a325025d6
TamirisLinard <102989908+TamirisLinard@users.noreply.github.com>
2024-11-21 09:17:41 -0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      // Adicione plugins aqui se necessário
+      ['module-resolver', {
+        alias: {
+          '@': './src', // Cria um alias para facilitar importações
+        },
+      }],
+      'react-native-reanimated/plugin' // Necessário para o React Native Reanimated
+    ],
   };
 };
